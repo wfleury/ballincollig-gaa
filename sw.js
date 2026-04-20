e8d1be062ce0823f216bb8cbbf074fe96db2eb91
wfleury <wfleury@users.noreply.github.com>
2026-04-20 15:42:29 +0000

    deploy: 7acf17eebadfae913dbe437e31374d8a51a07751

---
+++ sw.js
@@ -0,0 +1,35 @@
+// Service worker for Ballincollig GAA dashboard
+const CACHE = 'gaa-dash-1776699606';
+const CORE = ['./', './index.html', './img/crest.gif', './manifest.webmanifest'];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE).then((c) => c.addAll(CORE)).then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) => Promise.all(
+      keys.filter((k) => k !== CACHE).map((k) => caches.delete(k))
+    )).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  if (e.request.method !== 'GET') return;
+  const url = new URL(e.request.url);
+  if (url.origin !== self.location.origin) return;
+  e.respondWith(
+    caches.match(e.request).then((cached) => {
+      const network = fetch(e.request).then((res) => {
+        if (res && res.status === 200) {
+          const copy = res.clone();
+          caches.open(CACHE).then((c) => c.put(e.request, copy));
+        }
+        return res;
+      }).catch(() => cached);
+      return cached || network;
+    })
+  );
+});
