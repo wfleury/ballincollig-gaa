ded5d2d279610df70438bcd82911f53165bab674
wfleury <wfleury@users.noreply.github.com>
2026-05-01 13:45:30 +0000

    deploy: 4411ddf05d8b4939107f65497478fbc6c5aae05c

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Service worker for Ballincollig GAA dashboard
-const CACHE = 'gaa-dash-1777620241';
+const CACHE = 'gaa-dash-1777642987';
 const CORE = ['./img/crest.gif', './manifest.webmanifest'];
 
 self.addEventListener('install', (e) => {
